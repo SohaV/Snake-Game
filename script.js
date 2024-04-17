4280a3afc87af7244d2280933472878837c063e1
SohaV <vankudresoha12@gmail.com>
2024-04-17 18:14:17 -0400

    Handled snake movement to not change to opposite direction

--- script.js
+++ script.js
@@ -32,26 +32,30 @@ function update() {
     context.fillStyle = "#dde5b6";
     context.fillRect(0, 0, board.width, board.height);
 
+    context.fillStyle = "#bc4749";
+    context.fillRect(foodX, foodY, blocksize, blocksize);
+
+    if (snakeX == foodX && snakeY == foodY) {
+        placeFood();
+    }
+
     context.fillStyle = "#386641";
     snakeX += velocityX * blocksize;
     snakeY += velocityY * blocksize;
     context.fillRect(snakeX, snakeY, blocksize, blocksize);
-
-    context.fillStyle = "#bc4749";
-    context.fillRect(foodX, foodY, blocksize, blocksize);
 }
 
 function changeDirection(e) {
-    if (e.code == "ArrowUp") {
+    if (e.code == "ArrowUp" && velocityY != 1) {
         velocityX = 0;
         velocityY = -1;
-    } else if (e.code == "ArrowDown") {
+    } else if (e.code == "ArrowDown" && velocityY != -1) {
         velocityX = 0;
         velocityY = 1;
-    } else if (e.code == "ArrowLeft") {
+    } else if (e.code == "ArrowLeft" && velocityX != 1) {
         velocityX = -1;
         velocityY = 0;
-    } else if (e.code == "ArrowRight") {
+    } else if (e.code == "ArrowRight" && velocityX != -1) {
         velocityX = 1;
         velocityY = 0;
     }
